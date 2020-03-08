b005eff7ddabd9a88303cd405f43a844e1134d45
fursuli <48065322+fursuli@users.noreply.github.com>
2020-03-08 11:38:28 +0200

    initial

--- 05.03.2020/task_1.py
+++ 05.03.2020/task_1.py
@@ -1,1 +1,26 @@
-11001
+"""
+ Create class Person with AbcMethods (personal_info, personal_age).
+ Create child classes: Entrant(last_name, birth_date, faculty),
+ Student(last_name, birth_date, faculty, year_of_studying),
+ Teacher(last_name, birth_date, faculty, position, experience).
+
+ Create list of n persons, display full info from base AND make to possible
+ searching persons which required for asked diapason.
+ """
+from abc import ABC, abstractmethod
+
+
+class AbstractPerson(ABC):
+
+    @abstractmethod
+    def personal_info(self):
+        pass
+
+    @abstractmethod
+    def birth_date(self):
+        pass
+
+    @abstractmethod
+    def personal_age(self):
+        pass
+
