4b450b498d69908238730e4bf5235214388cc0d8
fursuli <48065322+fursuli@users.noreply.github.com>
2020-03-08 12:07:30 +0200

    initial

---
+++ 05.03.2020/task_2.py
@@ -0,0 +1,10 @@
+"""
+Create network: classes with required functions
+(Authorization(Registration)), add password validation(contains
+symbols and numbers), unique login.
+User can sign up(login, password, repeat password) -> user sign in.
+User can logout, sign in as another user.
+Class User(1 - admin, 2 - common user),
+Common User - can post(with date),
+Admin - can view all users, they registration date and posts.
+"""
